5ec2d59a9f8f3e3a92a8e8bffdf544ee4211e5c4
liujixu <1111@qq.com>
2019-06-17 19:08:39 +0800

    提交项目

---
+++ git_test/src/main/java/cn/itcast/lyyzk/test/GitTest.java
@@ -0,0 +1,16 @@
+package cn.itcast.lyyzk.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+        System.out.println("dsadfasfas");
+
+    }
+}
