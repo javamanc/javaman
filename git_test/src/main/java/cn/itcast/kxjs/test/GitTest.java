eb4f0b27e1913e081c6072a28e1938745ba4fdf5
liujixu <1111@qq.com>
2019-06-17 20:16:50 +0800

    Revert "提交"
    This reverts commit e7b31802

---
+++ git_test/src/main/java/cn/itcast/kxjs/test/GitTest.java
@@ -0,0 +1,16 @@
+package cn.itcast.kxjs.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+        System.out.println("daaaaaaaaaaa");
+
+    }
+}
